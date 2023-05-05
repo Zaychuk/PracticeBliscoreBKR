0e7641148d93be3acda2543db8e61466992284f4
Oleh Zaiats <oleh@veni.no>
2023-05-05 23:28:40 +0300

    Initial commit with mvp products logic

---
+++ PracticeBliscoreBKR/Controllers/ProductsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticeBliscoreBKR.Dtos;
+using PracticeBliscoreBKR.Services;
+
+namespace PracticeBliscoreBKR.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpPost("create/default")]
+    public void CreateDefaultProducts()
+    {
+        _productService.CreateDefaultProducts();
+    }
+
+    [HttpGet("all")]
+    public List<ProductDto> GetAllProducts()
+    {
+        var products = _productService.GetAllProducts();
+
+        return products;
+    }
+
+    [HttpGet("byName")]
+    public ProductDto GetProductByName(string name)
+    {
+        var product = _productService.GetProductByName(name);
+
+        return product;
+    }
+
+    [HttpGet("isAvailable")]
+    public bool CheckIfProductIsAvailable(string name)
+    {
+        var product = _productService.GetProductByName(name);
+
+        return product.IsAvailable;
+    }
+
+    [HttpGet("byManufacturer")]
+    public List<ProductDto> GetProductsByManufacturer(string manufacturer)
+    {
+        var products = _productService.GetProductsByManufacturer(manufacturer);
+
+        return products;
+    }
+
+    [HttpGet("byCategoty")]
+    public List<ProductDto> GetProductsByCategory(string category)
+    {
+        var products = _productService.GetProductsByCategory(category);
+
+        return products;
+    }
+}
---
+++ PracticeBliscoreBKR/Services/ProductService.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using PracticeBliscoreBKR.Dtos;
+using PracticeBliscoreBKR.Repositories;
+
+namespace PracticeBliscoreBKR.Services;
+
+public interface IProductService
+{
+    bool CheckIfProductIsAvailable(string name);
+    void CreateDefaultProducts();
+    List<ProductDto> GetAllProducts();
+    ProductDto GetProductByName(string name);
+    List<ProductDto> GetProductsByCategory(string category);
+    List<ProductDto> GetProductsByManufacturer(string manufacturer);
+}
+
+public class ProductService : IProductService
+{
+    private readonly IProductRepository _productRepository;
+    private readonly IMapper _mapper;
+
+    public ProductService(IProductRepository productRepository, IMapper mapper)
+    {
+        _productRepository = productRepository;
+        _mapper = mapper;
+    }
+
+    public void CreateDefaultProducts()
+    {
+        _productRepository.CreateDefaultProducts();
+    }
+
+    public List<ProductDto> GetAllProducts()
+    {
+        var products = _productRepository.GetAllProducts();
+
+        return products.Select(product => _mapper.Map<ProductDto>(product)).ToList();
+    }
+
+    public ProductDto GetProductByName(string name)
+    {
+        var product = _productRepository.GetProductByName(name);
+
+        return _mapper.Map<ProductDto>(product);
+    }
+
+    public bool CheckIfProductIsAvailable(string name)
+    {
+        var product = _productRepository.GetProductByName(name);
+
+        return _mapper.Map<ProductDto>(product).IsAvailable;
+    }
+
+    public List<ProductDto> GetProductsByManufacturer(string manufacturer)
+    {
+        var products = _productRepository.GetProductsByManufacturer(manufacturer);
+
+        return products.Select(product => _mapper.Map<ProductDto>(product)).ToList();
+    }
+
+    public List<ProductDto> GetProductsByCategory(string category)
+    {
+        var products = _productRepository.GetProductsByCategory(category);
+
+        return products.Select(product => _mapper.Map<ProductDto>(product)).ToList();
+    }
+}
